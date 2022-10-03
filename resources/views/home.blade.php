505d5ce00066fb66f64a3d665631262a778ca99b
DikaNizzer <dikanizzer28@gmail.com>
2022-10-03 11:01:38 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TedXUnair</title>
+    <link rel="stylesheet" href="loader/style.css">
+</head>
+<body>
+    <div class="loader">
+        <span>T</span>
+        <span>E</span>
+        <span>D</span>
+        <span>X</span>
+        <span>U</span>
+        <span>N</span>
+        <span>A</span>
+        <span>I</span>
+        <span>R</span>
+    </div>
+</body>
+</html>
