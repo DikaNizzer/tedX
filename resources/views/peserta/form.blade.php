06e4881d8153ce378c21921e49c47cca33860043
talitaaulia91 <90740632+talitaaulia91@users.noreply.github.com>
2022-10-26 20:20:03 +0700

    update view dashboard peserta

--- resources/views/peserta/form.blade.php
+++ resources/views/peserta/form.blade.php
@@ -5,32 +5,29 @@
   <div class="header">
     <div class="header-background"></div>
     <div class="container">
-      <div class="card card-profile-bottom">
+      <div class="card-profile-bottom">
         <div class="card-body p-3">
           <div class="row gx-4">
             <div class="col-auto">
-              <div class="avatar avatar-xl position-relative">
+              {{-- <div class="avatar avatar-xl position-relative">
                 <img src="/images/team-1.jpg" alt="profile_image" width="80" class="rounded-circle">
-              </div>
+              </div> --}}
             </div>
             <div class="col-auto my-auto">
               <div class="h-100">
                 <h5 class="mb-1 fw-bold">
-                  Halo, emailpeserta@gmail.com
+                  Welcome, {{ Auth::user()->email }}
                 </h5>
-                {{-- <p class="mb-0 text-sm">
-                  emailpeserta@gmail.com
-                </p> --}}
-                
               </div>
             </div>
             <div class="col-lg-4 col-md-6 my-sm-auto ms-sm-auto me-sm-0 mx-auto mt-4">
-              <div class="nav-wrapper position-relative end-0" style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
-                <ul class="breadcrumb breadcrumb-pills ms-lg-auto ms-md-auto ms-sm-0 my-auto py-2 px-3" >
-                  <li class="breadcrumb-item-active"><a href="/home" class="text-dark">Dashboard</a></li> 
-                  {{-- <li class="breadcrumb-item-active"><a href="/home" class="text-dark">Profile</a></li> --}}
-                  <li class="breadcrumb-item-active"><a href="/status" class="text-dark">Status Pendaftaran</a></li>
-                </ul>
+              <div class="row d-flex justify-content-center">
+                <div class="col-auto d-flex justify-content-end">
+                  <a href="/home" class="profile-menu-1">Dashboard</a>
+                </div>
+                <div class="col-auto  d-flex justify-content-start">
+                  <a href="/status" class="profile-menu-2">Registration Status</a>
+                </div>
               </div>
             </div>
           </div>
@@ -42,28 +39,10 @@
   <div class="event my-4">
     <div class="container">
       <div class="row row-cols-1 row-cols-md-2 g-4">
-        {{-- <div class="col-lg-12">
-          <div class="card h-100">
-            <img src="images/main-event.png" class="card-img-top zoom" alt="header-main-event">
-            <div class="card-body h-100">
-              <h3 class="card-title fw-bold mb-4">MAIN EVENT</h3>
-              <h5 class="card-subtitle mb-2" style="font-weight: 600">SESSION OF TALKS BASED ON TEDxUniversitasAirlangga Talks</h5>
-              <p class="card-text">TEDx events include live speakers, live experiences, recorded TED Talks, etc</p>
-            </div>
-            <div class="card-body button">
-              <button class="btn-custom-mainev disabled">Coming soon</button>
-            </div>
-            <div class="card-footer">
-              <small class="text-muted">Registration: DD-MM-YYYY</small>
-              <br>
-              <small class="text-muted">Location: Universitas Airlangga Kampus B </small>
-            </div>
-          </div>
-        </div> --}}
         <div class="col-lg-12">
-          <div class="card h-100">
+          <div class="card h-100" >
             <img src="images/sub-event.png" class="card-img-top zoom" alt="header-sub-event">
-            <div class="card-body h-100">
+            <div class="card-body h-100"  style="padding:7%; padding-top:10px">
               <h3 class="card-title fw-bold mb-4">SUB EVENT</h3>
               <h5 class="card-subtitle mb-2" style="font-weight: 600">WANDERLUST</h5>
               <p class="card-text">A medium to display various works of experience that can be felt by humans with their five
@@ -80,71 +59,84 @@
                 <input class="form-control" type="hidden" name="angkatan" value="{{ $peserta->first()->angkatan }}" id="fakultas">
                 <input class="form-control" type="hidden" name="alamat" value="{{ $peserta->first()->alamat }}" id="alamat">
 
-                <div class="form-group">
+                <div class="form-group mt-3">
                   <label for="kontak" class="form-control-label">Contact info (id line/no wa) </label>
-                  <input class="form-control" type="text" name="kontak" value="Kontak" id="kontak">
+                  <input class="form-control peserta-form" type="text" name="kontak" value="Kontak" id="kontak">
                 </div>
-                <div class="form-group">
+                <div class="form-group mt-3">
                   <label for="akun_ig" class="form-control-label">Instagram account </label>
-                  <input class="form-control" type="text" name="akun_ig" value="Instagram" id="akun_ig">
+                  <input class="form-control peserta-form" type="text" name="akun_ig" value="Instagram" id="akun_ig">
                 </div>
                 <input type="hidden" value="{{ $peserta->first()->user_id }}" name="user_id">
                 <input type="hidden" value="2" name="event_id">
-                <div class="form-group">
+                <div class="form-group mt-3">
                   <label for="link_gdrive" class="form-control-label">link folder gdrive</label>
-                  <p><i>link folder gdrive (Name_Faculty; e.g., Dira_FKM) which contains video (Video_Name; e.g., Video_Dira), 
+                  <p style="font-size:12px"><i>link folder gdrive (Name_Faculty; e.g., Dira_FKM) which contains video (Video_Name; e.g., Video_Dira), 
                     CV (CV_Name; e.g., CV_Dira), KTM (KTM_Name; e.g., KTM_Dira), Script talks (Script_Name; e.g., Script_Dira)</i></p>
-                  <input class="form-control" type="text" name="link_gdrive" value="Link Google Drive" id="link_gdrive">
+                  <input class="form-control peserta-form" type="text" name="link_gdrive" value="Link Google Drive" id="link_gdrive">
                 </div>
                 @else
-                <div class="form-group">
-                  <label for="nama" class="form-control-label">Full Name</label>
-                  <input class="form-control" type="text" name="nama" value="{{ old('nama') }}" id="nama">
+
+                <div class="row">
+                  <div class="col-lg-6" style="padding:20px">
+                    <div class="form-group mt-3">
+                      <label for="nama" class="form-control-label">Full Name</label>
+                      <input class="form-control peserta-form" type="text" name="nama" value="{{ old('nama') }}" id="nama">
+                    </div>
+                    <div class="form-group mt-3">
+                      <label for="nim" class="form-control-label">Student ID</label>
+                      <input class="form-control peserta-form" type="text" name="nim" value="{{ old('nim') }}" id="nim">
+                    </div>
+                    <div class="form-group mt-3">
+                      <label for="fakultas" class="form-control-label">Faculty</label>
+                      <input class="form-control peserta-form" type="text" name="fakultas" value="{{ old('fakultas') }}" id="fakultas">
+                    </div>
+                    <div class="form-group mt-3">
+                      <label for="angkatan">Batch</label>
+                      <select class="form-control peserta-form" name="angkatan" id="angkatan">
+                        <option value="18" {{ (old('angkatan') == "18") ? 'selected' : '' }}>2018</option>
+                        <option value="19" {{ (old('angkatan') == "19") ? 'selected' : '' }}>2019</option>
+                        <option value="20" {{ (old('angkatan') == "20") ? 'selected' : '' }}>2020</option>
+                        <option value="21" {{ (old('angkatan') == "21") ? 'selected' : '' }}>2021</option>
+                      </select>
+                    </div>
+                    <div class="form-group mt-3">
+                      <label for="alamat" class="form-control-label">Address </label>
+                      <input class="form-control peserta-form" type="text" name="alamat" value="{{ old('alamat') }}" id="alamat">
+                    </div>
+                  </div>
+                  <div class="col-lg-6" style="padding:20px">
+                    <div class="form-group mt-3">
+                      <label for="kontak" class="form-control-label">Contact info (id line/no wa) </label>
+                      <input class="form-control peserta-form" type="text" name="kontak" value="{{ old('kontak') }}" id="kontak">
+                    </div>
+                    <div class="form-group mt-3">
+                      <label for="akun_ig" class="form-control-label">Instagram account </label>
+                      <input class="form-control peserta-form" type="text" name="akun_ig" value="{{ old('akun_ig') }}" id="akun_ig">
+                    </div>
+                    <input type="hidden" value="{{ Auth::user()->id }}" name="user_id">
+                    <input type="hidden" value="2" name="event_id">
+                  
+                    <div class="form-group mt-3">
+                      <label for="link_gdrive" class="form-control-label">link folder gdrive</label>
+                      <p style="font-size:12px"><i>link folder gdrive (Name_Faculty; e.g., Dira_FKM) which contains video (Video_Name; e.g., Video_Dira), 
+                        CV (CV_Name; e.g., CV_Dira), KTM (KTM_Name; e.g., KTM_Dira), Script talks (Script_Name; e.g., Script_Dira)</i></p>
+                      <input class="form-control peserta-form" type="text" name="link_gdrive" value="{{ old('link_gdrive') }}" id="link_gdrive">
+                    </div>
+                  </div>
                 </div>
-                <div class="form-group">
-                  <label for="nim" class="form-control-label">Student ID</label>
-                  <input class="form-control" type="text" name="nim" value="{{ old('nim') }}" id="nim">
-                </div>
-                <div class="form-group">
-                  <label for="fakultas" class="form-control-label">Faculty</label>
-                  <input class="form-control" type="text" name="fakultas" value="{{ old('fakultas') }}" id="fakultas">
-                </div>
-                <div class="form-group">
-                  <label for="angkatan">Batch</label>
-                  <select class="form-control" name="angkatan" id="angkatan">
-                    <option value="18" {{ (old('angkatan') == "18") ? 'selected' : '' }}>2018</option>
-                    <option value="19" {{ (old('angkatan') == "19") ? 'selected' : '' }}>2019</option>
-                    <option value="20" {{ (old('angkatan') == "20") ? 'selected' : '' }}>2020</option>
-                    <option value="21" {{ (old('angkatan') == "21") ? 'selected' : '' }}>2021</option>
-                  </select>
-                </div>
-                <div class="form-group">
-                  <label for="alamat" class="form-control-label">Address </label>
-                  <input class="form-control" type="text" name="alamat" value="{{ old('alamat') }}" id="alamat">
-                </div>
-                <div class="form-group">
-                  <label for="kontak" class="form-control-label">Contact info (id line/no wa) </label>
-                  <input class="form-control" type="text" name="kontak" value="{{ old('kontak') }}" id="kontak">
-                </div>
-                <div class="form-group">
-                  <label for="akun_ig" class="form-control-label">Instagram account </label>
-                  <input class="form-control" type="text" name="akun_ig" value="{{ old('akun_ig') }}" id="akun_ig">
-                </div>
-                <input type="hidden" value="{{ Auth::user()->id }}" name="user_id">
-                <input type="hidden" value="2" name="event_id">
-              
-                <div class="form-group">
-                  <label for="link_gdrive" class="form-control-label">link folder gdrive</label>
-                  <p><i>link folder gdrive (Name_Faculty; e.g., Dira_FKM) which contains video (Video_Name; e.g., Video_Dira), 
-                    CV (CV_Name; e.g., CV_Dira), KTM (KTM_Name; e.g., KTM_Dira), Script talks (Script_Name; e.g., Script_Dira)</i></p>
-                  <input class="form-control" type="text" name="link_gdrive" value="{{ old('link_gdrive') }}" id="link_gdrive">
-                </div>
+
+
+
+
+
+
                 @endif
                 
                 <div class="card-body button">
                   <div class="dropdown">
                     {{-- <button type="submit" class="btn-custom-subev" data-bs-toggle="modal" data-bs-target="#aturanModal">Submit</button> --}}
-                    <button type="submit" class="btn-custom-subev">Submit</button>
+                    <button type="submit" class="btn-custom-subev">SUBMIT</button>
                   </div>
                 </div>
 
